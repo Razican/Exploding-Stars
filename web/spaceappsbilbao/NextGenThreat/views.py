1945cac26bb7b88ae5fcceca9a1150110117056e
Razican <admin@razican.com>
2015-04-11 02:44:14 +0200

    Added credits view, still descriptions missings

--- web/spaceappsbilbao/NextGenThreat/views.py
+++ web/spaceappsbilbao/NextGenThreat/views.py
@@ -1,3 +1,5 @@
+#-*-*- encoding: utf-8 -*-*-
+
 from django.shortcuts import render
 from django.template import RequestContext, loader, Context
 
@@ -13,5 +15,11 @@ def radar(request):
 	return render(request, 'NextGenThreat/radar.html', context)
 
 def credits(request):
-	context = {'name': 'Stephane'}
+	context = {'contributors': [
+			{'name': 'Aitor', 'lastname': 'Brazaola', 'description': 'Aitor Brazaola nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'},
+			{'name': 'Elena', 'lastname': 'López', 'description': 'Elena López nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'},
+			{'name': 'Iban', 'lastname': 'Eguia', 'description': 'Iban Eguia nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'},
+			{'name': 'Eneko', 'lastname': 'Cruz', 'description': 'Eneko Cruz nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'},
+		],
+	}
 	return render(request, 'NextGenThreat/credits.html', context)
