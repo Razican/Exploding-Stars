006b221c9a927adb3ffff7481ff80b718f2e275e
Razican <admin@razican.com>
2015-04-12 13:07:41 +0200

    Fixed description

--- web/spaceappsbilbao/NextGenThreat/views.py
+++ web/spaceappsbilbao/NextGenThreat/views.py
@@ -20,7 +20,7 @@ def credits(request):
 			{'name': 'Eneko', 'lastname': 'Cruz', 'description': 'Eneko is studying Computer Science at the University of Deusto and Mathematics at the National University of Distance Education (UNED). His main interests are information security, mathematics and computer vision.'},
 			{'name': 'Elena', 'lastname': 'López', 'description': 'Elena is a student at University of Deusto. She is studying Telecom Engineering and is very interested in fields such as Internet security, biomedicine and aeronautics. She is currently working on OpenStratos to send a Raspberry Pi to the stratosphere.'},
 			{'name': 'Iban', 'lastname': 'Eguia', 'description': 'Iban is a future IT engineer and a total space geek. Translator and contributor at CodeIgniter and core developer at OpenStratos and XG Project. He has a podcast with Aitor Brazaola called El Gato de Turing.'},
-			{'name': 'Alejandro', 'lastname': 'Pérez', 'description': 'Alejandro is a last year software engineering student, researcher in bioinformatics and net security and and cofounder of aprenditeka.'},
+			{'name': 'Alejandro', 'lastname': 'Pérez', 'description': 'Alejandro is a last year software engineering student, researcher in bioinformatics and net security and cofounder of aprenditeka.'},
 		],
 	}
 	return render(request, 'NextGenThreat/credits.html', context)
