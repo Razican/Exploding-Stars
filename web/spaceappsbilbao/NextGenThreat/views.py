f22ec691fbea3dc5d89d3a1b5b85ef1dbceac4be
Razican <admin@razican.com>
2015-04-12 13:06:32 +0200

    Added description for Alejandro

--- web/spaceappsbilbao/NextGenThreat/views.py
+++ web/spaceappsbilbao/NextGenThreat/views.py
@@ -16,11 +16,11 @@ def radar(request):
 
 def credits(request):
 	context = {'contributors': [
-			{'name': 'Aitor', 'lastname': 'Brazaola', 'description': 'Aitor Brazaola is a student of Computer Engineering. Been a geek and software developer, he has a podcast with Iban Eguia named El Gato de Turing.'},
+			{'name': 'Aitor', 'lastname': 'Brazaola', 'description': 'Aitor is a student of Computer Engineering. Been a geek and software developer, he has a podcast with Iban Eguia named El Gato de Turing.'},
+			{'name': 'Eneko', 'lastname': 'Cruz', 'description': 'Eneko is studying Computer Science at the University of Deusto and Mathematics at the National University of Distance Education (UNED). His main interests are information security, mathematics and computer vision.'},
 			{'name': 'Elena', 'lastname': 'López', 'description': 'Elena is a student at University of Deusto. She is studying Telecom Engineering and is very interested in fields such as Internet security, biomedicine and aeronautics. She is currently working on OpenStratos to send a Raspberry Pi to the stratosphere.'},
-			{'name': 'Iban', 'lastname': 'Eguia', 'description': 'Iban Eguia is a future IT engineer and a total space geek. Translator and contributor at CodeIgniter and core developer at OpenStratos and XG Project. He has a podcast with Aitor Brazaola called El Gato de Turing.'},
-			{'name': 'Eneko', 'lastname': 'Cruz', 'description': 'Eneko Cruz is studying Computer Science at the University of Deusto and Mathematics at the National University of Distance Education (UNED). His main interests are information security, mathematics and computer vision.'},
-			{'name': 'Alejandro', 'lastname': 'Pérez', 'description': 'Alejandro Pérez...'},
+			{'name': 'Iban', 'lastname': 'Eguia', 'description': 'Iban is a future IT engineer and a total space geek. Translator and contributor at CodeIgniter and core developer at OpenStratos and XG Project. He has a podcast with Aitor Brazaola called El Gato de Turing.'},
+			{'name': 'Alejandro', 'lastname': 'Pérez', 'description': 'Alejandro is a last year software engineering student, researcher in bioinformatics and net security and and cofounder of aprenditeka.'},
 		],
 	}
 	return render(request, 'NextGenThreat/credits.html', context)
